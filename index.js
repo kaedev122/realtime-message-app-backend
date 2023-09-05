5dd101aebec61ee46b04e834af227626229b293c
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-09-05 13:21:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.listen(3001);
