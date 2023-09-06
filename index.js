644ce5b539ea5f5e474d205e045da4a5365fcbac
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-09-06 22:11:48 +0700

    api login, register

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const multer = require("multer");
+const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const postRoute = require("./routes/posts");
+// const conversationRoute = require("./routes/conversations");
+// const messageRoute = require("./routes/messages");
+const router = express.Router();
+const path = require("path");
 
-app.listen(3001);
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true, useUnifiedTopology:true}).then( () => {
+    console.log(`Mongodb connected!`);
+}).catch( (err) => {
+    console.log(err);
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/conversations", conversationRoute);
+// app.use("/api/messages", messageRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Backend server is running at port: ${process.env.PORT}!`);
+});
