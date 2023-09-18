54cb85aa7f1ff1199d5803b4e2341f626df6819d
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-09-18 23:08:10 +0700

    cors update

--- index.js
+++ index.js
@@ -21,13 +21,12 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-app.use(
-    cors({
-        origin: ['https://realtime-message-app-backend.vercel.app'],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true,
-    })
-);
+app.use(cors());
+// {
+//     origin: ['https://realtime-message-app-backend.vercel.app'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+// }
 app.use(cookieParser());
 
 app.use(express.json());
