b9d169e5f193b894deb408d69a1a1e9d755c447e
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-10-09 21:40:41 +0700

    fix api

--- Controllers/ConversationController.js
+++ Controllers/ConversationController.js
@@ -21,8 +21,22 @@ module.exports.getAllConversation = async (req, res) => {
         const conversation = await Conversation.find({
             members: { $in: [req.user._id.toHexString()] },
         });
-        console.log(conversation)
-        res.status(200).json(conversation);
+        
+        const result = await Promise.map(conversation, async (item) => {
+            const usersData = await User.find({ _id: { $in: item.members } });
+            const users = usersData.map(user => ({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                profilePicture: user.profilePicture,
+            }));
+            return {
+                _id: item._id,
+                createdAt: item.createdAt,
+                members: users
+            }
+        })
+        res.status(200).json(result);
     } catch (err) {
         res.status(500).json(err);
     }
