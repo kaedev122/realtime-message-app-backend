27d42db2c075d9bb486ac7eaa86dc67629c8c66a
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-10-09 20:28:52 +0700

    fix bug

--- Controllers/ConversationController.js
+++ Controllers/ConversationController.js
@@ -3,8 +3,8 @@ const Conversation = require("../Models/Conversation.js");
 const Promise = require('bluebird');
 
 module.exports.createConversation =  async (req, res) => {
-    const sender = mongoose.Types.ObjectId(req.body.senderId)
-    const receiver = mongoose.Types.ObjectId(req.body.receiverId)
+    const sender = req.body.senderId
+    const receiver = req.body.receiverId
     const newConversation = new Conversation({
         members: [sender, receiver],
     });
@@ -29,8 +29,8 @@ module.exports.getAllConversation = async (req, res) => {
 
 module.exports.getOneConversation = async (req, res) => {
     try {
-        const firstUserId = mongoose.Types.ObjectId(req.params.firstUserId);
-        const secondUserId = mongoose.Types.ObjectId(req.params.secondUserId)
+        const firstUserId = req.params.firstUserId
+        const secondUserId = req.params.secondUserId
         const conversation = await Conversation.findOne({
             members: { $all: [firstUserId, secondUserId] },
         });
