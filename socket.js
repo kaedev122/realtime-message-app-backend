b42c43ef41f4b6d20f2edd55360ed6f0b7031550
DESKTOP-2FFGKUH\kaedev <fongnguyen122@gmail.com>
2023-10-21 22:37:42 +0700

    test

--- socket.js
+++ socket.js
@@ -14,14 +14,36 @@ module.exports = async function connectSocket(http) {
     const removeUser = (socketId) => {
         users = users.filter(user => user.socketId !== socketId)
     }
+
+    const getUser = (userId) => {
+        return users.find((user) => user.userId === userId);
+    };
     
     io.on("connection", (socket) => {
         console.log(`⚡: ${socket.id} user just connected!`);
+
         socket.on("addUser", userId => {
             addUser(userId, socket.id)
             io.emit("getUsers", users)
         })
-    
+
+        socket.on("sendMessage", ({ _id, conversationId, createdAt, image, sender, members }) => {
+            const mems = members.map(member => {
+                return member._id
+            })
+            const usersGetMessage = mems.map(mem => {
+                return getUser(mem).socketId
+            });
+
+            io.to(usersGetMessage).emit("getMessage", {
+                _id: _id,
+                conversationId: conversationId,
+                createdAt: createdAt,
+                image: image,
+                sender: sender
+            });
+        });
+
         socket.on("disconnect", () => {
             console.log(`❌: ${socket.id} user just disconnected!`)
             removeUser(socket.id)
